2b93dada44f970ab1cc1b48333ecc17ce017b944
TiirthDev <88479708+TiirthDev@users.noreply.github.com>
2022-06-26 09:30:35 +0530

    Update HeroBanner.js

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -4,7 +4,7 @@ import { Box, Stack, Typography } from '@mui/material';
 import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
-    <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Box sx={{ mt: { lg: '212px', xs: '170px' }, ml: { sm: '50px' } }} position="relative" p="20px">
         <Typography sx={{ mt: '-160px' }} color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
         <Typography fontWeight={700} sx={{ fontSize: { lg: '38px', xs: '30px' } }} mb="10px" mt="10px">
             Sweat, Smile <br />
